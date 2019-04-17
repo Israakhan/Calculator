fd67eae388e1cd0550e452905a8f0fe1db8c4cd6
Israakhan <47556456+Israakhan@users.noreply.github.com>
2019-04-17 02:55:14 -0700

    printing the choices

---
+++ Calc.java
@@ -0,0 +1,18 @@
+class Calc{
+
+public static void main(String arg[]){
+
+System.out.println("1-Addition");
+System.out.println("2-substraction");
+System.out.println("1-multiplication");
+System.out.println("1-divison");
+System.out.println("1-modulation");
+
+
+
+
+
+}
+
+
+}
